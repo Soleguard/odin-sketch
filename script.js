513bcad247040e1de26d73ff8a1e4d0c74832f66
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-30 12:48:40 -0500

    Include container and loop
    This version provides the reference to the DOM container that manages each square produced. A nested for-loop iterates between rows and columns 16 by 16 times, creating squares.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Reference container
+const CONTAINER = document.getElementById("container");
 
+// Iterate through rows
+for (let row = 0; row < 16; row++)
+{
+    // Iterate through columns
+   for (let col = 0; col < 16; col++)
+    {
+        // Create square div
+        let square = document.createElement("div");
+
+        // Set class
+        square.classList.add("square");
+
+        // Add to container
+        CONTAINER.appendChild(square);
+    }
+}
