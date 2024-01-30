5d92e46675597cd175bb40885bcc3768b222aa51
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-30 14:13:29 -0500

    Edit classes and row loop
    This version attempts to resolve the single-column issue by copying the square production code from the column loop into the row loop. In addition, it renames the previous square class to the respective row-square and column-square class names.

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ const CONTAINER = document.getElementById("container");
 // Iterate through rows
 for (let row = 0; row < 16; row++)
 {
-    // Iterate through columns
+    // Create row square
+    let row_Square = document.createElement("div");
+
+    // Set class
+    row_Square.classList.add("row-square");
+
+    // Add to container
+    CONTAINER.appendChild(row_Square);
+
+    // Iterate through columns per row
    for (let col = 0; col < 16; col++)
     {
-        // Create square div
-        let square = document.createElement("div");
+        // Create column square
+        let row_Column = document.createElement("div");
 
         // Set class
-        square.classList.add("square");
+        row_Column.classList.add("row-column");
 
         // Add to container
-        CONTAINER.appendChild(square);
+        CONTAINER.appendChild(row_Column);
     }
 }
