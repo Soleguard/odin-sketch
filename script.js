c36d26e419e6d226cf1186ad8899679d11fa47e0
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-02-08 10:58:27 -0500

    Add button, create and remove grid functions, and square load constants
    This version adds a button to edit square load, create and remove grid functions, and constants for default square load and maximum square load.

--- script.js
+++ script.js
@@ -1,45 +1,87 @@
-// Reference container
+// Reference container, button, and default and max loop iterations
 const CONTAINER = document.getElementById("container");
+const BUTTON = document.getElementById("selectable");
+const DEFAULT_TIMES = 16;
+const MAX_TIMES = 100;
 
-// Loop 16 times
-for (let iteration = 0; iteration < 16; iteration++)
+// Add function to create grid
+function create_Grid(times)
 {
-    // Create column to occupy squares
-    let column_Container = document.createElement("ul");
-    // Add to main container
-    CONTAINER.appendChild(column_Container);
-
-    // Create 16 vertical squares per iteration
-    for (let col = 0; col < 16; col++)
+    // Loop depending on times
+    for (let iteration = 0; iteration < times; iteration++)
     {
-        // Create list item for each square
-        let list_Item = document.createElement("li");
-
-        // Create square
-        let square = document.createElement("div");
-        // Set class
-        square.classList.add("square");
-        // Add square to list item
-        list_Item.appendChild(square);
-
-        // Add list item to column container
-        column_Container.appendChild(list_Item);
-
-        // Add event listener for mouseover for any square
-        square.addEventListener('mouseover', function(e)
+        // Create vertical square per iteration
+        for (let col = 0; col < times; col++)
         {
-            // Reference former background color
-            old_Background_Color = e.target.style.backgroundColor;
+            // Create square
+            let square = document.createElement("div");
+            // Set class
+            square.classList.add("square");
+            // Add square to list item
+            CONTAINER.appendChild(square);
 
-            // Set background color to gray
-            e.target.style.backgroundColor = 'gray';
-
-            // Add event listener for mouseleave in this square
-            e.target.addEventListener('mouseleave', function(e)
+            // Add event listener for mouseover for any square
+            square.addEventListener('mouseover', function(e)
             {
-                // Restore former background color
-                e.target.style.backgroundColor = old_Background_Color;
+                // Reference former background color
+                old_Background_Color = e.target.style.backgroundColor;
+
+                // Set background color to gray
+                e.target.style.backgroundColor = 'gray';
+
+                // Add event listener for mouseleave in this square
+                e.target.addEventListener('mouseleave', function(e)
+                {
+                    // Restore former background color
+                    e.target.style.backgroundColor = old_Background_Color;
+                });
             });
-        });
+        }
     }
-}
+};
+
+// Add function to remove grid
+function remove_Grid()
+{
+    // Remove each child
+    while (CONTAINER.firstChild)
+    {
+        CONTAINER.removeChild(CONTAINER.firstChild);
+    }
+};
+
+// Create default grid
+create_Grid(DEFAULT_TIMES);
+
+// Add event listener function on button click
+BUTTON.addEventListener('click', function()
+{
+    // Prompt for choice and store into variable
+    let choice = prompt("Enter a new grid value");
+
+    // Check if canceled
+    if (choice == null || choice == '')
+    {
+        // Exit function
+        return;
+    }
+
+    // Convert user input to number
+    choice = Number(choice);
+
+    // Verify limit
+    if (choice <= MAX_TIMES)
+    {
+        // Remove current grid
+        remove_Grid();
+
+        // Generate new grid according to user input
+        create_Grid(choice);
+    }
+
+    // Display error message
+    else
+    {
+        alert('Limit is 100 x 100 squares.');
+    }
+});
